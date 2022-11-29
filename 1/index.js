f534e2c83351709b8652348b429c09077df659f7
Gastón Quiroga <gasquiroga1@gmail.com>
2022-11-29 20:55:30 -0300

    clase 1

---
+++ 1/index.js
@@ -0,0 +1,54 @@
+// 1)
+
+
+// if ( colorSemaforo  === "verde" ) {
+//     console.log ( true )
+// } else {
+//     console.log ( "Error: No puede avanzar" )
+// }
+
+
+// 2)
+
+
+// if ( letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u" ||) {
+//     console.log ( "Es una vocal")
+// } else {
+//     console.log ( "No es una vocal")
+// }
+
+// const expresionRegular = /[aeiouAEIOU]/ ---> NO ES UNA STRING, ES UNA EXPRESION REGULAR
+
+// if ( letra.match (expresionRegular ) ) {
+//     console.log ( "Es una vocal" )
+// } else {
+//     console.log ( "No es una vocal" )
+// }
+
+
+// 3)
+
+
+// const expresionConsonantes = /[bcdfghjklmnñpqrstvwxyz]/ {
+//     console.log ( true )
+// } else {
+//     console.log ( false )
+// }
+
+
+// 5)
+
+
+// if ( numero % 2 === 0 ) {
+//     console.log ( "Es par")
+// } else {
+//     console.log ( "Es impar")
+// }
+
+// 6)
+
+// if ( numero >=1 ) {
+//     console.log ( "Es positivo")
+// } else {
+//     console.log ( "Es negativo")
+// }
---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("¡MIRÁ MAMÁAA, ESTOY PROGRAMANDO!") 
+
+const par = "22" 
+
+console.log (par)
+
+const impar = "75"
+
+console.log (impar)
+
+const suma = (par + impar)
+
+console.log(suma) 
+
+const nombreDelUsuario = prompt ( "INGRESE SU NOMBRE" )
+
+console.log(`Bienvenide a nuestra web ${ nombreDelUsuario }`)
+
+const multiplicacion = ( 5 * impar )
+
+console.log ( multiplicacion )
+
+const usuario1 = "Alejandro"
+const usuario2 = "Mateo"
+const usuario3 = "Gastón"
+
+console.log( usuario1 + ", " +  usuario2 + " y " + usuario3 )
+
+console.log ( multiplicacion - 100 )
+
+console.log ( typeof "par" ) 
+
+console.log ( typeof "impar" ) 
